ad0ec0bee14eba360af1662f082d5ad00763252d
Dinuka De Silva <L.dinukadesilva@gmail.com>
2026-03-05 16:30:59 -0500

    CTT-871: Adding dashboard auth interceptor to the cider axios instance

--- src/contexts/auth/DashboardAuthenticator.js
+++ src/contexts/auth/DashboardAuthenticator.js
@@ -12,28 +12,33 @@ export const dashboardAxiosInstance = axios.create({
     baseURL: window.SETTINGS.OPERATIONS_API_BASE_URL + window.SETTINGS.OPERATIONS_API_INTEGRATION_BADGES_PATH
 });
 
-dashboardAxiosInstance.interceptors.request.use(
-    async function (config) {
-        if (window.SETTINGS.DISABLE_DASHBOARD_AUTHENTICATION === false) {
-            let newToken;
-            try {
-                newToken = await getNewToken();
-            } catch (e) {
-                // newToken = "<no-valid-token-received-from-dashboard>"
-                window.location = "/login?next=" + window.location.pathname;
+addDashboardAuthInterceptor(dashboardAxiosInstance);
+addDashboardAuthInterceptor(unauthorizedCiderAxiosInstance);
+
+function addDashboardAuthInterceptor(axiosInstance) {
+    axiosInstance.interceptors.request.use(
+        async function (config) {
+            if (window.SETTINGS.DISABLE_DASHBOARD_AUTHENTICATION === false) {
+                let newToken;
+                try {
+                    newToken = await getNewToken();
+                } catch (e) {
+                    // newToken = "<no-valid-token-received-from-dashboard>"
+                    window.location = "/login?next=" + window.location.pathname;
+                }
+
+                // Update the Authorization header
+                config.headers.Authorization = `Bearer ${newToken}`;
             }
 
-            // Update the Authorization header
-            config.headers.Authorization = `Bearer ${newToken}`;
+            return config;
+        },
+        function (error) {
+            // Handle request errors
+            return Promise.reject(error);
         }
-
-        return config;
-    },
-    function (error) {
-        // Handle request errors
-        return Promise.reject(error);
-    }
-);
+    );
+}
 
 async function getNewToken() {
     try {
