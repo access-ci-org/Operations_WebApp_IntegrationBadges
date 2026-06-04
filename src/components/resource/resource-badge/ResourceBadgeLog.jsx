897596339fdc63ea60906bca3118a4f2b90b7a7d
Dinuka De Silva <L.dinukadesilva@gmail.com>
2026-06-04 11:43:53 -0400

    Style improvements to the badge activity log

--- src/components/resource/resource-badge/ResourceBadgeLog.jsx
+++ src/components/resource/resource-badge/ResourceBadgeLog.jsx
@@ -27,13 +27,18 @@ export default function ResourceBadgeLog({resourceId, roadmapId, badgeId}) {
                 <div className="text-end pe-3" style={{minWidth: 200, maxWidth: 200}}>
                     <BadgeStatus status={status}/>
                 </div>
-                <div className="flex-fill border-start border-gray-200 border-2">
-                    <div className="pb-2 ps-3">
-                        {comment && <div className="w-100 fs-7 d-flex flex-row">
+                <div className="flex-fill border-start border-gray-200">
+                    <div
+                        className="w-100 mt-1 ps-3 fs-9 text-start text-gray-600">{lastUpdatedAt.toLocaleString()} by {lastUpdatedBy}</div>
+                    <div className="w-100 border-2 ps-3">
+                        {comment && <div className="w-100 pt-2 fs-7 d-flex flex-row">
                             <i className="bi bi-chat-right-text-fill pe-2 text-gray-500"></i>
-                            <span className="pre-wrap-text text-break">{comment}</span>
+                            <div>
+                                <span
+                                    className="pre-wrap-text text-break ps-2 pe-2 pt-1 pb-1 d-inline-block bg-gray-100 rounded rounded-2">
+                                    {comment}</span>
+                            </div>
                         </div>}
-                        <div className="w-100 mt-2 fs-9 text-start text-gray-600">{lastUpdatedAt.toLocaleString()} by {lastUpdatedBy}</div>
                     </div>
                 </div>
             </div>
