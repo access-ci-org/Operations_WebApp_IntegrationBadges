13aa493a465dee04336d73620ba05a663733566f
Dinuka De Silva <L.dinukadesilva@gmail.com>
2026-06-11 12:18:48 -0400

    Refactoring

--- src/contexts/reducers/DefaultReducer.js
+++ src/contexts/reducers/DefaultReducer.js
@@ -1,10 +1,10 @@
 export default function DefaultReducer(state, newState) {
-    if (Array.isArray(state)) {
+    if (state === null || state === undefined) {
+        return newState
+    } else if (Array.isArray(state)) {
         return [
             ...newState
         ]
-    } else if (state === null || state === undefined) {
-        return newState
     } else if (typeof state === "object") {
         return {
             ...newState
